b77d68332afc6005ceb29ddf3e527417f68aec04
Chris LaRose <cjlarose@gmail.com>
2017-08-06 21:54:19 -0700

    Read length-prefixed messages in a forever-loop

--- app/Main.hs
+++ app/Main.hs
@@ -7,16 +7,21 @@ import Data.ByteString (hGet, hPut)
 import System.IO (IOMode(ReadWriteMode), hClose)
 import Data.Binary.Strict.Get (runGet, getWord32be)
 import Data.Int (Int32)
+import Control.Monad (forever)
 
 runConn :: (Socket, SockAddr) -> IO ()
 runConn (sock, _) = do
     handle <- socketToHandle sock ReadWriteMode
-    len <- hGet handle 4
-    let (res, _) = runGet getWord32be len
-    case res of
-      Left err -> return ()
-      Right lenAsWord -> hPut handle . fromString . show $ (fromIntegral lenAsWord :: Int32)
-    hClose handle
+    forever $ do
+      len <- hGet handle 4
+      let (res, _) = runGet getWord32be len
+      case res of
+        Left err -> return ()
+        Right lenAsWord -> do
+          let msgLen = fromIntegral lenAsWord :: Int32
+          hPut handle . fromString . show $ msgLen
+          msg <- hGet handle . fromIntegral $ msgLen
+          hPut handle . fromString . show $ msg
 
 mainLoop :: Socket -> IO ()
 mainLoop sock = do
