0a8af54616a55d8177eb64de9653b996b213d3ac
Chris LaRose <cjlarose@gmail.com>
2017-08-13 11:36:33 -0700

    Parse metadata requests

--- app/Main.hs
+++ app/Main.hs
@@ -1,22 +1,27 @@
 module Main where
 
-import Data.Int (Int16, Int32)
+import Data.Int (Int32)
 import Network.Socket hiding (send, recv)
 import Network.Socket.ByteString (send, recv)
 import Data.ByteString.UTF8 (fromString)
 import Data.ByteString (hGet, hPut, ByteString)
 import System.IO (IOMode(ReadWriteMode), hClose)
 import Data.Binary.Strict.Get (runGet, getWord32be)
-import Data.Int (Int32)
 import Control.Monad (forever)
-import KafkaMessage (requestMessageHeader)
+import KafkaMessage (KafkaRequest(..), kafkaRequest)
 import Data.Attoparsec.ByteString (parseOnly, endOfInput)
+
+respondToRequest :: KafkaRequest -> ByteString
+respondToRequest (TopicMetadataRequest topics) = fromString . maybe "no topics" show $ topics
 
 handleRequest :: ByteString -> ByteString
 handleRequest request =
-  case (parseOnly (requestMessageHeader <* endOfInput) request) of
+  case (parseOnly (kafkaRequest <* endOfInput) request) of
     Left err -> fromString "Oops"
-    Right (apiKey, apiVersion, correlationId, clientId) -> fromString "success"
+    Right req ->
+      case req of
+        Left err -> fromString err
+        Right validReq -> respondToRequest validReq
 
 runConn :: (Socket, SockAddr) -> IO ()
 runConn (sock, _) = do
@@ -42,6 +47,6 @@ main :: IO ()
 main = do
     sock <- socket AF_INET Stream 0
     setSocketOption sock ReuseAddr 1
-    bind sock (SockAddrInet 4242 iNADDR_ANY)
+    bind sock (SockAddrInet 9092 iNADDR_ANY)
     listen sock 2
     mainLoop sock
--- src/KafkaMessage.hs
+++ src/KafkaMessage.hs
@@ -1,8 +1,9 @@
-module KafkaMessage (requestMessageHeader) where
+module KafkaMessage (KafkaRequest(..), kafkaRequest) where
 
+import Data.Maybe (catMaybes)
 import Data.Int (Int16, Int32)
 import Data.ByteString.UTF8 (toString)
-import Data.Attoparsec.ByteString (Parser, take)
+import Data.Attoparsec.ByteString (Parser, take, count)
 import Data.Attoparsec.Binary (anyWord16be, anyWord32be)
 
 signedInt16be :: Parser Int16
@@ -20,7 +21,31 @@ kafkaString = do
     str <- Data.Attoparsec.ByteString.take . fromIntegral $ len
     return . Just . toString $ str
 
+kafkaArray :: Parser a -> Parser (Maybe [a])
+kafkaArray p = do
+  len <- signedInt32be
+  if (len < 0) then do
+    return Nothing
+  else do
+    xs <- count (fromIntegral len) p
+    return . Just $ xs
+
+data KafkaRequest = TopicMetadataRequest (Maybe [String])
+
+metadataRequest :: Parser KafkaRequest
+metadataRequest = (TopicMetadataRequest . (<$>) catMaybes) <$> kafkaArray kafkaString
+
 requestMessageHeader :: Parser (Int16, Int16, Int32, Maybe String)
 requestMessageHeader =
   (\apiKey apiVersion correlationId clientId -> (apiKey, apiVersion, correlationId, clientId))
     <$> signedInt16be <*> signedInt16be <*> signedInt32be <*> kafkaString
+
+kafkaRequest :: Parser (Either String KafkaRequest)
+kafkaRequest = do
+  (apiKey, apiVersion, correlationId, clientId) <- requestMessageHeader
+  case apiKey of
+    3 -> do
+      request <- metadataRequest
+      return . Right $ request
+    _ -> do
+      return . Left $ "Unknown request type"
