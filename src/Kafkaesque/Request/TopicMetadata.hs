985c3ee0dd7d601d69469d88ab4f3e9b56ee90ed
Chris LaRose <cjlarose@gmail.com>
2017-09-04 15:31:40 -0700

    Correctly handle metadata requests that specify topics names

--- src/Kafkaesque/Request/TopicMetadata.hs
+++ src/Kafkaesque/Request/TopicMetadata.hs
@@ -2,13 +2,16 @@ module Kafkaesque.Request.TopicMetadata
   ( metadataRequestV0
   ) where
 
+import Control.Arrow (second)
 import Data.Attoparsec.ByteString (Parser)
+import Data.Int (Int64)
 import qualified Data.Pool as Pool
 import qualified Database.PostgreSQL.Simple as PG
 
-import Kafkaesque.KafkaError (noError)
+import Kafkaesque.KafkaError (noError, unknownTopicOrPartition)
 import Kafkaesque.Parsers (kafkaArray, kafkaString)
-import Kafkaesque.Queries (getAllTopicsWithPartitionCounts)
+import Kafkaesque.Queries
+       (getAllTopicsWithPartitionCounts, getPartitionCount, getTopicId)
 import Kafkaesque.Request.KafkaRequest
        (KafkaRequest, KafkaResponseBox(..), respond)
 import Kafkaesque.Response
@@ -21,9 +24,22 @@ newtype TopicMetadataRequestV0 =
 metadataRequestV0 :: Parser TopicMetadataRequestV0
 metadataRequestV0 = TopicMetadataRequestV0 <$> kafkaArray kafkaString
 
+getTopicWithPartitionCount :: PG.Connection -> String -> IO (Maybe Int64)
+getTopicWithPartitionCount conn topicName = do
+  topicIdRes <- getTopicId conn topicName
+  maybe (return Nothing) (fmap Just . getPartitionCount conn) topicIdRes
+
+getTopicsWithPartitionCounts ::
+     PG.Connection -> [String] -> IO [(String, Maybe Int64)]
+getTopicsWithPartitionCounts conn =
+  mapM
+    (\topicName -> do
+       count <- getTopicWithPartitionCount conn topicName
+       return (topicName, count))
+
 respondToRequest ::
      Pool.Pool PG.Connection -> TopicMetadataRequestV0 -> IO KafkaResponseBox
-respondToRequest pool (TopicMetadataRequestV0 ts) = do
+respondToRequest pool (TopicMetadataRequestV0 requestedTopics) = do
   let brokerNodeId = 42
   let brokers = [Broker brokerNodeId "localhost" 9092]
   let makePartitionMetadata partitionId =
@@ -33,12 +49,20 @@ respondToRequest pool (TopicMetadataRequestV0 ts) = do
           brokerNodeId
           [brokerNodeId]
           [brokerNodeId]
-  let makeTopicMetadata (name, partitionCount) =
+  let makeTopicMetadata (name, Nothing) =
+        TopicMetadata unknownTopicOrPartition name []
+      makeTopicMetadata (name, Just partitionCount) =
         TopicMetadata
           noError
           name
           (map makePartitionMetadata [0 .. (partitionCount - 1)])
-  topics <- Pool.withResource pool getAllTopicsWithPartitionCounts
+  let getTopics :: PG.Connection -> Maybe [String] -> IO [(String, Maybe Int64)]
+      getTopics conn topics =
+        case topics of
+          Nothing -> map (second Just) <$> getAllTopicsWithPartitionCounts conn
+          Just [] -> map (second Just) <$> getAllTopicsWithPartitionCounts conn
+          Just xs -> getTopicsWithPartitionCounts conn xs
+  topics <- Pool.withResource pool (`getTopics` requestedTopics)
   let topicMetadata = map makeTopicMetadata topics
   return . KResp $ TopicMetadataResponseV0 brokers topicMetadata
 
